c7dc5a0246da8b456e56596887061287e6b0b646
Steve Wagner <scalawagz@outlook.com>
2015-10-22 17:02:37 -0700

    Making the compile script handle all the things

--- compile.sh
+++ compile.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 echo Transpiling styles...
-sass --update ./src/main/assets/styles/
+sass --update ./src/main/
 
 echo Transpiling scripts...
-coffee -c -m ./src/main/assets/scripts/
+coffee -c -m ./src/main/
 
 echo Transpiling test scripts...
 coffee -c -m ./src/test/spec/
